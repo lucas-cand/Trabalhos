f86f5957d7e61b2e50f466ebe9dce53150748ddc
lucas-cand <lucascandido2007@outlook.com>
2026-04-29 20:45:35 -0400

    Create Exercicio6.js

---
+++ Exercicio6.js
@@ -0,0 +1,14 @@
+let = nivel prompt("Escolha o nivel");
+switch (nivel){
+  case 1:
+    console.log("Iniciante");
+    break;
+
+  case 2:
+    console.log("Intermediario");
+    break;
+
+  case 3:
+    console.log("Avançado");
+    break;
+}
