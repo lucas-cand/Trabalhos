6f4f93422536f3d5effc83302ccf61a9a86f34c8
lucas-cand <lucascandido2007@outlook.com>
2026-04-29 20:32:37 -0400

    Create Exercicio3.js

---
+++ Exercicio3.js
@@ -0,0 +1,12 @@
+let numero = prompt("Digite um numero:");
+console.log("\nSeu numero é:");
+if (numero > 0 ){
+    console.log("Positivo")
+}
+if (numero == 0){
+    console.log("Zero")
+}
+if (numero < 0){
+    console.log("Negativo")
+}
+
