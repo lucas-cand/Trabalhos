e8b5905eba18cb583301a121797c18084cefdf6b
lucas-cand <lucascandido2007@outlook.com>
2026-04-29 20:57:47 -0400

    Update Exercicio5.js

--- Exercicio5.js
+++ Exercicio5.js
@@ -2,7 +2,7 @@ let nota = prompt ("Escreva a nota:")
 if (nota >= 7){
   console.log("Aprovado");
 } 
-if (nota >=5 &&  nota <7 ){
+if ((nota >=5) && (nota <7)){
   console.log("Recuperação");
 }  if (nota < 5) {
   console.log("Reprovado");
