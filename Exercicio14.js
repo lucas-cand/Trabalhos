ebbcd69646eb230927e020fc3e92a70a61a968e3
lucas-cand <lucascandido2007@outlook.com>
2026-05-20 21:34:31 -0400

    Create Exercicio14.js

---
+++ Exercicio14.js
@@ -0,0 +1,9 @@
+let X = Number(prompt ("Um numero:"));
+function Validacao(){
+    if ((X % 2) === 0 ){
+        console.log ("Par")
+    } else {
+        console.log("Impar")
+    }
+}
+Validacao()
